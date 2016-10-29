58f86402069d979b9b79a50a8578dccc184a64ca
Around Six <aroundsix@gmail.com>
2016-10-29 18:16:23 +0100

    the forgotten commit

--- src/components/Landing/LandingPage.js
+++ src/components/Landing/LandingPage.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import SignIn from '../SignIn.js';
+import SignUp from '../SignUp.js';
+import * as firebase from 'firebase';
 
 const LandingPage = React.createClass({
+
+  getUsers: function() {
+    const dbRef = firebase.database().ref().child('users');
+    dbRef.on('value', function(snap) {
+      console.log(snap.val());
+    })
+  },
   render: function () {
     return (
-      <div>
+      <div className="landing-page">
         <h1>Amiable</h1>
-        <p>Some PR text of somesort here</p>
-        <button>Sign Up</button>
-        <button>Sign In</button>
+        <SignIn />
       </div>
     )
   }
