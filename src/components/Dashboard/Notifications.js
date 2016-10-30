6f5d68cd69a8d0b85ae5ad8818dd2a1822326a99
Around Six <aroundsix@gmail.com>
2016-10-30 09:34:10 +0000

    *sighs* still working on accepting notifications

--- src/components/Dashboard/Notifications.js
+++ src/components/Dashboard/Notifications.js
@@ -2,27 +2,54 @@ import React from 'react';
 import * as firebase from 'firebase';
 
 const Notifications = React.createClass({
-  componentDidMount: function() {
+  getInitialState: function() {
+    return {
+      invitations: []
+    }
+  },
+  componentWillMount: function() {
     firebase.auth().onAuthStateChanged(function(firebaseUser) {
       const dbRef = firebase.database().ref().child('notifications');
+      var temp = [];
       dbRef.on('value', snap => {
+        temp = [];
         const notifications = snap.val();
         for(var key in notifications) {
           if(notifications[key].inviteeId === firebaseUser.displayName) {
-            console.log('WOGSDGHDAGADS')
+            temp.push({user: notifications[key].inviterId})
+            console.log(snap.key);
+            this.setState({invitations: temp})
           } else {
             console.log('I\'m done, goodbye, farewell!');
           }
         }
       })
-    })
+      this.handleInvitations(temp);
+    }.bind(this))
 
   },
+  handleInvitations: function(invitations) {
+    this.setState({invitations: invitations})
+  },
+  removeNotification: function() {
+    const dbRef = firebase.database().ref().child("notifications");
+    dbRef.on('child_removed', snap => {
+      const li = document.getElementById('')
+    })
+  },
   render: function () {
+    var nodes = this.state.invitations.map(function(invite, i) {
+      console.log(invite.snapKey);
+      return <li key={i}>
+        <p>You have a chat invitation from <strong> - {invite.user} - </strong></p>
+        <p>You can either <a href="#">Accept</a> or <button href="#">Decline</button></p>
+        </li>
+    })
     return (
       <div>
         <h2>Notifications</h2>
         <p>You currently have no notifications</p>
+        {nodes}
       </div>
     )
   }
